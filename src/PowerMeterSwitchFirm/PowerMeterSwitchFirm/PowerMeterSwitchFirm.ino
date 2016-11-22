af46503551a7714263dc35a509c67c2aeae7520a
Victor Ferrer <victorfergar@gmail.com>
2016-11-22 23:36:43 +0100

    PowerMeter_CS5460 library included

---
+++ src/PowerMeterSwitchFirm/PowerMeterSwitchFirm/PowerMeterSwitchFirm.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		PowerMeterSwitchFirm.ino
+ Created:	11/22/2016 11:36:12 PM
+ Author:	Victor
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
